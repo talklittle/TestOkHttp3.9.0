70257349152e3fab7922aec830f65705ca823097
Andrew Shu <talklittle@gmail.com>
2017-09-04 20:25:45 -0700

    Initial commit

---
+++ app/proguard-rules.pro
@@ -0,0 +1,4 @@
+
+-dontwarn okhttp3.*
+-dontwarn okio.*
+-dontwarn javax.annotation.**
---
+++ app/src/main/java/com/talklittle/testokhttp390/MainActivity.java
@@ -0,0 +1,56 @@
+package com.talklittle.testokhttp390;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.franmontiel.persistentcookiejar.ClearableCookieJar;
+import com.franmontiel.persistentcookiejar.PersistentCookieJar;
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ClearableCookieJar cookieJar =
+                new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(this));
+        OkHttpClient client = new OkHttpClient.Builder()
+                .cookieJar(cookieJar)
+                .build();
+        Request request = new Request.Builder()
+                .url("https://www.google.com/")
+                .header("User-Agent", "com.talklittle.testokhttp390")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                Log.w("MainActivity", e);
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                ResponseBody body = response.body();
+                if (body != null) {
+                    Log.i("MainActivity", body.string());
+                    body.close();
+                }
+            }
+        });
+    }
+}
